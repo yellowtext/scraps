443a5bc6321a30bd2e808c51b21bfb77e36bdcb0
Nikolay Skovorodin <kolyanchik@live.ru>
2018-01-08 20:13:47 +0700

    Now it works

--- meetingplace.py
+++ meetingplace.py
@@ -14,6 +14,9 @@ class report:
         self.task_time = task_time
         self.additional = additional
 
+    def __str__(self):
+        return self.report_date + " " + self.author + " " + self.task + " " + self.task_time + " " + self.quote_id + " "+ self.additional
+
 class affirmation:
     def __init__(self, affirmation_date, affirmation_text):
         self.affirmation_date = affirmation_date
@@ -24,37 +27,37 @@ class lastts:
         self.last_time = last_time
 
 class DBConnector:
-    def readQuote (fetchrow):
+    def readQuote (self, fetchrow):
         quotes_date = str(fetchrow[1], "utf-8")
         quotes_text = str(fetchrow[2], "utf-8")
         obj_quote = quotes(quotes_date, quotes_text)
         return obj_quote
 
-    def readReport (fetchrow):
-        quote_id = str(fetchrow[1], "utf-8")
-        report_date = str(fetchrow[2], "utf-8")
+    def readReport (self,fetchrow):
+        quote_id = fetchrow[1]
+        report_date = fetchrow[2]
         author = str(fetchrow[3], "utf-8")
-        task = fetchrow[4].decode('cp1251')
-        task_time = str(fetchrow[5], "utf-8")
-        additional = str(fetchrow[6], "cp1251")
+        task = str(fetchrow[4], "utf-8")
+        task_time = fetchrow[5]
+        additional = str(fetchrow[6], "utf-8")
         obj_report = report(quote_id, report_date, author, task, task_time, additional)
         return obj_report
 
-    def readAffirmation (fetchrow):
+    def readAffirmation (self,fetchrow):
         affirmation_date = str(fetchrow[1], "utf-8")
         affirmation_text = str(fetchrow[2], "utf-8")
         obj_affirmation = affirmation(affirmation_date, affirmation_text)
         return obj_affirmation
 
-    def readLastts (fetchrow):
+    def readLastts (self, fetchrow):
         last_time = str(fetchrow[1], "utf-8")
         obj_lastts = lastts(last_time)
         return obj_lastts
 
 if __name__ == '__main__':
-    db = _mysql.connect(host="localhost", user="root", passwd="Ghjuhfvvbhjdfybt72", db="meetingplace")
+    db = _mysql.connect(host="localhost", user="root", passwd="toor", db="meetingplace")
     db.query('SELECT * FROM reports')
     r = db.use_result()
 
     readR = DBConnector()
-    print(readR.readReport(r.fetch_row()))
+    print(readR.readReport(r.fetch_row()[0]))
